9d61617fc4e3f453a4258ca11f834665fcf4a3aa
/ohnsonWTC <johnsondubula@gmail.com>
2022-11-12 00:35:28 +0200

    Day11 revesion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DoorComponent } from './door/door.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DoorComponent
   ],
   imports: [
     BrowserModule,
