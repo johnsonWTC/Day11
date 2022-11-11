339320cee05690cd528add6e88fac3f0bef00020
/ohnsonWTC <johnsondubula@gmail.com>
2022-11-12 00:37:39 +0200

    Day11 revesion

--- src/app/door/door.component.ts
+++ src/app/door/door.component.ts
@@ -1,10 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+
+export interface Door{
+  size :number,
+  color : string
+}
+
 @Component({
   selector: 'app-door',
   templateUrl: './door.component.html',
   styleUrls: ['./door.component.css']
 })
+
+
+
 export class DoorComponent implements OnInit {
 
   constructor() { }
